b9e616bfd24bc249d1c3db9a3be965f924c48a0a
developer <mousesnake9@gmail.com>
2022-09-22 05:08:28 -0700

    add card

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import data from './data';
+import Card from 'react-bootstrap/Card';
+
+function App() {
+  return (
+    <div>
+      <header></header>
+      <main>
+        <h1>Image Slicer</h1>
+        <div className="users">
+          {data.users.map((user) => (
+            <div className="user" key={user.Route_ID}>
+              <Card onClick={test(user.Route_ID)}>
+                <img src={user.URL} alt={user.Name} />
+                <div className="user-info">
+                  <p>{user.Name}</p>
+                  <p>{user.Route_ID}</p>
+                  <p>{user.Freq}</p>
+                </div>
+              </Card>
+            </div>
+          ))}
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/data.js
@@ -0,0 +1,30 @@
+const data = {
+  users: [
+    {
+      Route_ID: 1,
+      URL: 'https://i.imgur.com/ym1OK1A.png',
+      Name: 'Mary Jones',
+      Freq: 25,
+    },
+    {
+      Route_ID: 2,
+      URL: 'https://i.imgur.com/icKRIOb.png',
+      Name: 'Steve Smith',
+      Freq: 12,
+    },
+    {
+      Route_ID: 3,
+      URL: 'https://i.imgur.com/qSkW7Iz.png',
+      Name: 'David Jackson',
+      Freq: 34,
+    },
+    {
+      Route_ID: 4,
+      URL: 'https://i.imgur.com/i2X6mve.png',
+      Name: 'Trevor Greggs',
+      Freq: 54,
+    },
+  ],
+};
+
+export default data;
